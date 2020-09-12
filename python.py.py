3a0a35d1757096b7fa05d60659ad9b3ba401a7d0
A01702762 <69860878+A01702762@users.noreply.github.com>
2020-09-11 20:15:39 -0500

    Update python.py.py

--- python.py.py
+++ python.py.py
@@ -1,62 +1,47 @@
 #Georgina Alessabdra Corona Velázquez
 #A01702762
 #Calculadora de Indice de Masa Corporal(peso, edad, altura y genero)
+edad=int(input("¿Cuantos años tienes?"))
+estaturacm=float(input("¿Cual es tu estatura en cm?"))
+peso=float(input("¿Cual es tu peso en kg?"))
+genero=str(input("¿Eres hombre o mujer?"))
 
-
-
-#Crear una función llamada imc que va a mandar llamar la estatura y el peso
-
-print("¿Cuantos años tienes?")
-
-edad=int(input())
-
-print("¿Cual es tu estatura en cm?")
-
-estaturacm=float(input())
-estaturam=estaturacm/100
-
-print("¿Cual es tu peso en kg?")
-
-peso=float(input())
-
-print("¿Eres hombre o mujer?")
-
-genero=str(input())
+def estaturaenm(a):
+    return a/100
+estaturam = estaturaenm(estaturacm)
 
 def imc(a,b):
-  return a/b**2
- 
+    return a/b**2
 final = imc(peso,estaturam)
 
 print("Tu peso es de ", peso,"kg")
 print("Tu estatura es de ", estaturam,"m")
 print("Te identificas como ", genero)
 print("por lo tanto: ")
-
-
 print("Tu IMC es de: ", final)
 
+if(0<final<10):
+    print("Algo anda mal")
+    print("sugerencia: Revisa nuevamente los datos ingresados")
+elif(10<final<=16):
+  print("Tu IMC muestra delgadez severa")
+  print("sugerencia: consulte a su médico")
+elif(16<final<=17):
+  print("Tu IMC muestra delgadez moderada")
+  print("Sugerencia: Mantenga una dieta equilibrada")
+elif(17<final<=18.49):
+  print("Tu IMC muestra delgadez aceptable")
+  print("Sugerencia: Mantenga una dieta equilibrada")
+elif(18.49<final<=24.99):
+  print("Tu IMC muestra un peso normal")
+  print("Sugerencia: Ejercitese y tenga una dieta equilibrada")
+elif(24.99<final<=29.99):
+  print("Tu IMC muestra sobrepeso")
+  print("Sugerencia: Ejercitese, consulte a su medico y tenga una dieta equilibrada")
+elif(29.99<final<=35):
+  print("Tu IMC muestra obesidad")
+  print("Sugerencia: Consulte a su medico")
 
-
-if(24>=final>18):
-    
-    print("Tu IMC es normal")
-    print("sugerencia: haga ejercicio regularmente y alimentese sanamente")
-
-elif(final<18):
-    
-    print("Tu IMC es bajo")
-    print("Sugerencia: consulte a su médico")
-    
-
-elif(final >= 25 and final <30):
-    
-    print("Tu IMC es alto")
-    print("Sugerencia: consulte a su médico")
-
-elif(final >= 30):
-    print("Tu IMC muestra sobrepeso")
-    print("Sugerencia: Su salud peligra")
 
 
 
